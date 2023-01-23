a931246539fea1c73a52dac7a35c5f6a46941bc9
jeannot diambu <diambujeannot@gmail.com>
2023-01-23 21:28:37 +0100

    Add Signout and signin

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,22 @@
- import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react";
 
-const login = () => {
-  return (
-    <div>login</div>
-  )
-}
-
-export default login
- 
+const Login = () => {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign In</button>
+      </>
+    );
+  }
+};
+export default Login;
